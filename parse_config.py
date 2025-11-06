3a2d1e004f6cc197db375f12c2814a65eaf81237
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-11-06 13:17:44 +0100

    Add research interests to config file

--- parse_config.py
+++ parse_config.py
@@ -1,4 +1,6 @@
 import configparser
+from typing import List
+
 
 def get_config():
     config = configparser.ConfigParser()
@@ -8,3 +10,10 @@ def get_config():
 def get_api_key()->str:
     config = get_config()
     return config['gemini']['api_key']
+
+def get_interests()->List[str]:
+    config = get_config()
+    interests = config['topics']['research_interests']
+    interests = interests.split(',')
+    interests = [interest.strip() for interest in interests]
+    return interests
