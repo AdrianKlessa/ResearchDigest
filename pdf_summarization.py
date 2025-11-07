de8060006a54f8f5df556a53deb4bd100d592f71
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-11-07 15:52:26 +0100

    Implement paper summary / report functions

--- pdf_summarization.py
+++ pdf_summarization.py
@@ -1,5 +1,49 @@
 from text_cleaning import cleanup_article
 from pdf_text_extraction import get_pdf_text
+from model import get_llm
+from langchain_core.prompts import PromptTemplate
 
-def summarize_pdf(pdf_path: str)->str:
-    raise NotImplementedError
+summarize_prompt = PromptTemplate.from_template("Summarize the following arXiv article to 500 words or less: \n\n {article}")
+plain_english_prompt = PromptTemplate.from_template("Based on the following summary of an arXiv article, "
+                                             "write a simplified explanation that could be understandable to "
+                                             "an undergraduate student not acquainted well with this field: \n\n {article_summary}")
+pros_cons_prompt = PromptTemplate.from_template("Based on the following summary of an arXiv article, "
+                                                "write a list of positive and negative aspects of what was presented. "
+                                                "Mention potential applications, impact, as well as drawbacks or aspects "
+                                                "that might negatively influence wider application of what was presented (if applicable)."
+                                                "Article: \n\n {article_summary}")
+
+def summarize_article(article_text: str)->str:
+    llm = get_llm()
+    prompt = summarize_prompt.format(article=article_text)
+    result = llm.invoke(prompt)
+    result_text = result.content
+    return result_text
+
+def explain_summary(article_summary: str)->str:
+    llm = get_llm()
+    prompt = plain_english_prompt.format(article_summary=article_summary)
+    result = llm.invoke(prompt)
+    result_text = result.content
+    return result_text
+
+def explain_summary_pros_cons(article_summary: str)->str:
+    llm = get_llm()
+    prompt = pros_cons_prompt.format(article_summary=article_summary)
+    result = llm.invoke(prompt)
+    result_text = result.content
+    return result_text
+
+def print_pdf_report(pdf_path: str)->None:
+    extracted_text = get_pdf_text(pdf_path)
+    cleaned_text = cleanup_article(extracted_text)
+    article_summary = summarize_article(cleaned_text)
+    paper_explanation = explain_summary(article_summary)
+    pros_cons = explain_summary_pros_cons(article_summary)
+    print("------Final summary------")
+    print("Summary of the arXiv article:")
+    print(article_summary)
+    print("Simplified explanation:")
+    print(paper_explanation)
+    print("Potential pros and cons of the presented method:")
+    print(pros_cons)
